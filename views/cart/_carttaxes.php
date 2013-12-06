c97e68cae82991e85451470d8ae3f3200231d82e
Adam Simms <adam.simms@lightspeedretail.com>
2013-12-05 23:32:10 -0500

    WS-1071 Theme Refresh
    - Updated dark.css and light.css to reflect the layout changes of
    blue.css
    - Fixed products dropdown arrow
    - Fixed font display issue
    - Fixed advance search +

--- views/cart/_carttaxes.php
+++ views/cart/_carttaxes.php
@@ -2,7 +2,7 @@
 	<?php foreach($model->Taxes as $tax=>$taxvalue): ?>
 		<?php if($taxvalue): ?>
 			<div class="row-fluid">
-		        <div class="span2 offset8 cart_price"><span class="cart_label"><?= $tax; ?> Tax</span></div>
+		        <div class="span2 offset8 cart_price"><span class="cart_label"><?= $tax; ?></span></div>
 		        <div class="span2 cart_price"><?= _xls_currency($taxvalue); ?></div>
 			</div>
 		<?php endif; ?>
